021d08865770d86c03d7f7117b5cd35a396acc4d
JonathanC006 <114087493+JonathanC641@users.noreply.github.com>
2024-01-23 02:28:11 +0000

    Done with about and updates posts

--- src/components/about-box.js
+++ src/components/about-box.js
@@ -3,22 +3,25 @@ import "./css/about-box.css";
 const aboutBox = () => {
   return (
     <div className="About">
-      <div className="Header">About</div>
-      {/* <div className="Info"> */}
-      <div>Age: 12</div>
-      <div>Occupation: Pet</div>
-      <div>Education: WildCat Academy</div>
-      <div>
-        Favorite Bands: Cat Stevens, Def Leopard, Atomic Kitten, Lionel Ritchie
+      <div className="BackColor">
+        <div className="Header">About</div>
+        <div className="Info">Age: 12</div>
+        <div className="Info">Occupation: Pet</div>
+        <div className="Info">Education: WildCat Academy</div>
+        <div className="Info">
+          Favorite Bands: Cat Stevens, Def Leopard, Atomic Kitten, Lionel
+          Ritchie
+        </div>
+        <div className="Info">
+          Contact Info: Meow at me at the fence behind Burger King on 242nd and
+          Broadway
+        </div>
+        <div className="Info">Relationship Status: Single</div>
+        <div className="Info">
+          Places Lived: Paris, France and Bronx, New York
+        </div>
       </div>
-      <div>
-        Contact Info: Meow at me at the fence behind Burger King on 242nd and
-        Broadway
-      </div>
-      <div>Relationship Status: Single</div>
-      <div>Places Lived: Paris, France and Bronx, New York</div>
     </div>
-    // </div>
   );
 };
 
--- src/components/posts-box.js
+++ src/components/posts-box.js
@@ -4,44 +4,46 @@ const postsBox = () => {
   return (
     <div className="Posts">
       <div className="Header">
-        <div>Recent Posts</div>
+        <p>Recent Posts</p>
       </div>
-      <div>
-        <h5>Takota McConner Says:</h5>
-        <p>3 days ago</p>
-        <h5>Did nothing today, just laid around and ate food #bestdayever</h5>
-        <input type="text" placeholder="comment..." />
-      </div>
-      <div>
-        <h5>Takota McConner Says:</h5>
-        <p>4 days ago</p>
-        <h5>Didn't do much today, just layed around and ate food #yolo</h5>
-        <input type="text" placeholder="comment..." />
-      </div>
-      <div>
-        <h5>Takota McConner Says:</h5>
-        <p>7 days ago</p>
-        <h5>
-          Nothing accomplished today, just layed around and ate food
-          #livingmybestlife
-        </h5>
-        <input type="text" placeholder="comment..." />
-      </div>
-      <div>
-        <h5>Takota McConner Says:</h5>
-        <p>8 days ago</p>
-        <h5>Slept all day and ate a few meals #killingthegame</h5>
-        <input type="text" placeholder="comment..." />
-      </div>
-      <div>
-        <h5>Takota McConner Says:</h5>
-        <p>12 days ago</p>
-        <h5>
-          Was taken to the vet - considering puking in my owners shoes.
-          Opinions?
-        </h5>
-        <input type="text" placeholder="comment..." />
-      </div>
+      <ul>
+        <div>
+          <h5>Takota McConner Says:</h5>
+          <small>3 days ago</small>
+          <h6>Did nothing today, just laid around and ate food #bestdayever</h6>
+          <input type="text" placeholder="comment..." />
+        </div>
+        <div>
+          <h6>Takota McConner Says:</h6>
+          <small>4 days ago</small>
+          <h6>Didn't do much today, just layed around and ate food #yolo</h6>
+          <input type="text" placeholder="comment..." />
+        </div>
+        <div>
+          <h6>Takota McConner Says:</h6>
+          <small>7 days ago</small>
+          <h6>
+            Nothing accomplished today, just layed around and ate food
+            #livingmybestlife
+          </h6>
+          <input type="text" placeholder="comment..." />
+        </div>
+        <div>
+          <h6>Takota McConner Says:</h6>
+          <small>8 days ago</small>
+          <h6>Slept all day and ate a few meals #killingthegame</h6>
+          <input type="text" placeholder="comment..." />
+        </div>
+        <div>
+          <h6>Takota McConner Says:</h6>
+          <small>12 days ago</small>
+          <h6>
+            Was taken to the vet - considering puking in my owners shoes.
+            Opinions?
+          </h6>
+          <input type="text" placeholder="comment..." />
+        </div>
+      </ul>
     </div>
   );
 };
